bad3117bd846d4069a9d3e4a8589a5f958302f76
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2023-12-26 20:26:13 -0600

    Feat: Register functionality (save in db)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.drupal.org',
+                port: '',
 
+            }
+        ]
+    }
+}
+// https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png
 module.exports = nextConfig
